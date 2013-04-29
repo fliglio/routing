f88fc85fa47e8d4d8c8d77770b0a7419d0594d56
Ben Schwartz <ben.schwartz@bancvue.com>
2013-04-29 15:27:30 -0500

    updates

--- Fliglio/Routing/RestInvokerApp.php
+++ Fliglio/Routing/RestInvokerApp.php
@@ -23,12 +23,19 @@ class RestInvokerApp extends App {
 		
 		$method = $command;
 		if (substr($command, -1) == '!') {
+			$validMethod = in_array(HttpAttributes::getMethod(), array(
+				HttpAttributes::METHOD_POST, HttpAttributes::METHOD_GET, HttpAttributes::METHOD_PUT, HttpAttributes::METHOD_DELETE
+			));
+			if (!$validMethod) {
+				throw new CommandNotFoundException("HTTP Method '".HttpAttributes::getMethod()."' not supported");
+			}
+
 			$name = substr($command, 0, strlen($command)-1);
 			$method = HttpAttributes::getMethod() . ucFirst($name);
 		}
 		
 		if (!method_exists($instance, $method)) {
-			throw new CommandNotFoundException("Command '{$command}' does not exist (".$context->getRequest()->getCommand().")");
+			throw new CommandNotFoundException("Method '{$method}' does not exist (".$method.")");
 		}
 		
 		if (!($instance instanceof Routable)) {
